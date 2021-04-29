5ac1fdfc1f50f9b2185e3a1ff3f562793dd3dfa3
Clara-Kang <clara.kang@ubisoft.com>
2021-04-29 00:09:02 -0400

    add decal normal map, fix uv calculation, change models and textures

--- decal/myshaders.js
+++ decal/myshaders.js
@@ -83,13 +83,13 @@ const planeFragmentShader =
 `#version 300 es
 precision highp float;
 
-uniform sampler2D arrowTexture;
+uniform sampler2D colorTexture;
 
 in vec2 vUv;
 out vec4 fragmentColor;
 
 void main() {
-  vec4 color = texture(arrowTexture, vUv);
+  vec4 color = texture(colorTexture, vUv);
   fragmentColor = vec4(color.xyz, 0.5);
 }
 `
@@ -103,26 +103,26 @@ uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 in vec2 position;
-in vec3 spherePosition;
-in vec3 sphereNormal;
+in vec3 decalPosition;
+in vec3 decalNormal;
 in vec3 uDirection;
-in float sphereRadius;
+in float decalRadius;
 
 out vec3 vPosition;
-out vec3 vSpherePosition;
-out vec3 vSphereNormal;
-out float vSphereRadius;
+out vec3 vDecalPosition;
+out vec3 vDecalNormal;
+out float vDecalRadius;
 out vec3 vUdirection;
 
 void main() {
-  vec4 sphereViewPosition = modelViewMatrix * vec4(spherePosition, 1.0);
-  vec4 viewSpacePosition = vec4(sphereViewPosition.xy + position.xy * sphereRadius, sphereViewPosition.z, 1.0);
+  vec4 sphereViewPosition = modelViewMatrix * vec4(decalPosition, 1.0);
+  vec4 viewSpacePosition = vec4(sphereViewPosition.xy + position.xy * decalRadius, sphereViewPosition.z, 1.0);
   gl_Position = projectionMatrix * viewSpacePosition;
 
   vPosition = (inverse(viewMatrix) * viewSpacePosition).xyz;
-  vSpherePosition = spherePosition;
-  vSphereNormal = sphereNormal;
-  vSphereRadius = sphereRadius;
+  vDecalPosition = decalPosition;
+  vDecalNormal = decalNormal;
+  vDecalRadius = decalRadius;
   vUdirection = uDirection;
 }
 `
@@ -131,7 +131,9 @@ const decalFragmentShader =
 precision highp float;
 
 uniform sampler2D normalDepthTexture;
-uniform sampler2D arrowTexture;
+uniform sampler2D decalAlphaTexture;
+uniform sampler2D decalNormalTexture;
+uniform sampler2D decalColorTexture;
 
 uniform float ambient;
 uniform vec3 lightDirection;
@@ -141,29 +143,26 @@ uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 in vec3 vPosition;
-in vec3 vSphereNormal;
+in vec3 vDecalNormal;
 in vec3 vUdirection;
-in vec3 vSpherePosition;
-in float vSphereRadius;
+in vec3 vDecalPosition;
+in float vDecalRadius;
 out vec4 fragmentColor;
 
 vec3 worldPositionFromDepth(float depth) {
     vec2 texCoord = gl_FragCoord.xy / vec2(textureSize(normalDepthTexture, 0));
-
     float z = depth * 2.0 - 1.0;
     vec4 clipSpacePosition = vec4(texCoord * 2.0 - 1.0, z, 1.0);
     vec4 viewSpacePosition = inverse(projectionMatrix) * clipSpacePosition;
-
     viewSpacePosition /= viewSpacePosition.w;
-
     vec4 worldSpacePosition = inverse(viewMatrix) * viewSpacePosition;
 
     return worldSpacePosition.xyz;
 }
 
 void main() {
-  float distanceToSphereCenter = length(vPosition - vSpherePosition);
-  if (distanceToSphereCenter > vSphereRadius) {
+  float distanceToSphereCenter = length(vPosition - vDecalPosition);
+  if (distanceToSphereCenter > vDecalRadius) {
     discard;
   }
   vec2 texCoord = gl_FragCoord.xy/vec2(textureSize(normalDepthTexture, 0));
@@ -172,10 +171,10 @@ void main() {
     discard;
   }
   vec3 currentNormal = normalize(texture(normalDepthTexture, texCoord).xyz);
-  float lenOnPlaneNormal = sqrt(vSphereRadius*vSphereRadius - distanceToSphereCenter*distanceToSphereCenter);
-  vec3 planeNormal = normalize((inverse(viewMatrix) * vec4(0.0, 0.0, -1.0, 0.0)).xyz);
-  vec3 backPosition = vPosition + planeNormal*lenOnPlaneNormal;
-  vec3 frontPosition = vPosition - planeNormal*lenOnPlaneNormal;
+  float lenOnDecalPlaneNormal = sqrt(vDecalRadius*vDecalRadius - distanceToSphereCenter*distanceToSphereCenter);
+  vec3 decalPlaneNormal = normalize((inverse(viewMatrix) * vec4(0.0, 0.0, -1.0, 0.0)).xyz);
+  vec3 backPosition = vPosition + decalPlaneNormal*lenOnDecalPlaneNormal;
+  vec3 frontPosition = vPosition - decalPlaneNormal*lenOnDecalPlaneNormal;
 
   vec4 backPosInProjspace = projectionMatrix * viewMatrix * vec4(backPosition,1.0);
   vec4 frontPosInProjspace = projectionMatrix * viewMatrix * vec4(frontPosition,1.0);
@@ -187,22 +186,32 @@ void main() {
 
   if (frontDepth <= currentDepth && backDepth >= currentDepth) {
     vec3 currentWorldPos = worldPositionFromDepth(currentDepth);
-    vec3 centerToCurrentPos = currentWorldPos - vSpherePosition;
+    vec3 centerToCurrentPos = currentWorldPos - vDecalPosition;
     float centerToCurrentPosLen = length(centerToCurrentPos);
-    vec3 directionOnTangentPlane = centerToCurrentPos - vSphereNormal * dot(vSphereNormal, centerToCurrentPos);
-    float cosAngleWithUDir = dot(normalize(directionOnTangentPlane), vUdirection);
+    vec3 directionOnTangentPlane = centerToCurrentPos - vDecalNormal * dot(vDecalNormal, centerToCurrentPos);
 
-    float geodesisDistanceToCenter = (1.0 + pow(1.0 - dot(vSphereNormal, currentNormal), 4.0)) * centerToCurrentPosLen;
+    vec3 uDirection = vUdirection;
+    vec3 vDirection = normalize(cross(vDecalNormal, uDirection));
+    float cosAngleWithUDir = dot(normalize(directionOnTangentPlane), uDirection);
+    float cosAngleWithVDir = dot(normalize(directionOnTangentPlane), vDirection);
 
-    float u = (geodesisDistanceToCenter * sqrt(2.0) / vSphereRadius * cosAngleWithUDir + 1.0) / 2.0;
-    float v = (geodesisDistanceToCenter * sqrt(2.0) / vSphereRadius * sin(acos(cosAngleWithUDir)) + 1.0) / 2.0;
+    float geodesisDistanceToCenter = (1.0 + pow(1.0 - dot(vDecalNormal, currentNormal), 4.0)) * centerToCurrentPosLen;
 
-    float diffuse = max(dot(normalize(currentNormal), normalize(lightDirection)), 0.0);
-    float lightAmount = min(diffuse + ambient, 1.0);
+    vec2 uv = (vec2(cosAngleWithUDir, cosAngleWithVDir) * geodesisDistanceToCenter * sqrt(2.0) / vDecalRadius + 1.0) / 2.0;
 
-    if (u >= 0.0 && u <= 1.0 && v >= 0.0 && v <= 1.0) {
-      vec4 texColor = texture(arrowTexture, vec2(u, v));
-      fragmentColor = vec4(texColor.xyz * lightAmount, texColor.w);
+    if (uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0) {
+      vec3 normalTangentSpace = texture(decalNormalTexture, uv).xyz;
+      vec4 texColor = texture(decalColorTexture, uv);
+
+      normalTangentSpace = normalTangentSpace.xyz * 2.0 - 1.0;
+      vec3 normalWorldSpace = normalTangentSpace.x * uDirection - normalTangentSpace.y * vDirection + normalTangentSpace.z * vDecalNormal;
+
+      float diffuse = max(dot(normalWorldSpace, normalize(lightDirection)), 0.0);
+      float lightAmount = min(diffuse + ambient, 1.0);
+
+      float alpha = 1.0 - texture(decalAlphaTexture, uv).x;
+      fragmentColor = vec4(texColor.xyz * lightAmount, alpha);
+      // fragmentColor = vec4(vec3(lightAmount), alpha);
     }
   }
 }
