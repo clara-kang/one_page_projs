6f13f0405480faec1ce6771c5ca617f686763a2d
Clara-Kang <clara.kang@ubisoft.com>
2021-09-01 00:28:32 -0400

    add interaction to tree

---
+++ tree/shaders.js
@@ -0,0 +1,26 @@
+const interactionVertexShader =
+`#version 300 es
+precision highp float;
+
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+in vec3 position;
+
+void main() {
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`
+
+const interactionFragmentShader =
+`#version 300 es
+precision highp float;
+
+uniform vec2 interactionIndex;
+
+out vec4 fragmentColor;
+
+void main() {
+  fragmentColor = vec4(interactionIndex, 1.0, 1.0);
+}
+`
