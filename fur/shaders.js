a9f2e9eab23eddf2bc609994ad54827b8daaffb1
Clara-Kang <clara.kang@ubisoft.com>
2023-12-23 01:07:33 -0500

    use bunny, add dot generating script

--- fur/shaders.js
+++ fur/shaders.js
@@ -3,7 +3,7 @@ const FurVertexShader =
 precision highp float;
 
 const float radius = 1.0f;
-const float furMaxLength = 0.2f;
+const float furMaxLength = 0.1f;
 const float shellCount = 30.0f;
 const float shellThickness = furMaxLength / shellCount;
 const vec4  gravityDir = vec4(0, -1.0f, 0, 0);
@@ -39,9 +39,9 @@ const FurFragmentShader =
 `#version 300 es
 precision highp float;
 
-const float ambient = 0.0f;
+const float ambient = 0.1f;
 
-const vec3 light_beige_rgb = vec3(245.0, 245.0, 220.0) / 255.0;
+const vec3 light_beige_rgb = vec3(250.0, 240.0, 220.0) / 255.0;
 uniform sampler2D furTex;
 uniform sampler2D patternTex;
 uniform vec3 lightPos;
@@ -54,12 +54,55 @@ out vec4 fragmentColor;
 
 void main() {
   vec3 lightDir = normalize(lightPos - vPos);
-  //float diffuse = (dot(normalize(vNormal), vec3(1.0f)) + 1.0f) / 2.0f;
-  float diffuse = max(dot(normalize(vNormal), lightDir), 0.0f);
+  float diffuse = (dot(normalize(vNormal), lightDir) + 1.0f) / 2.0f;
+  //float diffuse = max(dot(normalize(vNormal), lightDir), 0.0f);
   float lightFrac = min(diffuse + ambient, 1.0f);
   vec4 color = texture(furTex, vUv * 8.0f);
   vec4 clippedColor = step(vec4(0.1f), color);
   vec4 patternColor = texture(patternTex, vUv * 8.0f);
-  fragmentColor = vec4(clippedColor.xyz * lightFrac * patternColor.xyz, clippedColor.w);
+  fragmentColor = vec4(clippedColor.xyz * lightFrac * light_beige_rgb, clippedColor.w);
+}
+`
+
+const BasicVertexShader =
+`#version 300 es
+precision highp float;
+
+uniform mat4 modelMatrix;
+uniform mat4 modelViewMatrix;
+uniform mat4 projectionMatrix;
+
+in vec3 normal;
+in vec3 position;
+out vec3 vNormal;
+out vec3 vPos;
+
+void main() {
+  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0f);
+  vNormal = vec3(modelMatrix * vec4(normal, 0));
+  vPos = vec3(modelMatrix * vec4(position, 1.0f));
+}
+`;
+
+const BasicFragmentShader =
+`#version 300 es
+precision highp float;
+
+const float ambient = 0.1f;
+const vec3 light_beige_rgb = vec3(250.0, 240.0, 220.0) / 255.0;
+
+uniform vec3 lightPos;
+
+in vec3 vNormal;
+in vec3 vPos;
+out vec4 fragmentColor;
+
+void main() {
+  vec3 lightDir = normalize(lightPos - vPos);
+  float diffuse = (dot(normalize(vNormal), lightDir) + 1.0f) / 2.0f;
+  //float diffuse = max(dot(normalize(vNormal), lightDir), 0.0f);
+  float lightFrac = min(diffuse + ambient, 1.0f);
+
+  fragmentColor = vec4(vec3(1.0f) * lightFrac * light_beige_rgb, 1.0f);
 }
 `
